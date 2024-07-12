2075cb8a47ef52f59852f2eecf6836d3054ea4ae
Mateo León <30735098+MateoLeon44@users.noreply.github.com>
2024-07-12 00:21:41 -0500

    Configured vite for building

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/grid/',
   css: {
     preprocessorOptions: {
       scss: {
@@ -23,5 +22,12 @@ export default defineConfig({
   },
   build: {
     minify: true,
+    target: 'esnext',
+    outDir: 'dist',
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
   },
 })
